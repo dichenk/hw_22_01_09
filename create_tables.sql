2c0b4312be8e11bbfc198413e6a2ee62f18b4c3f
dichenk <dchenk@DESKTOP-R5OCG73.localdomain>
2023-01-11 00:57:54 +0300

    build: first commit. Environment built

---
+++ create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees
+(
+	employee_id int2 PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int2 REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
